3e1b2f7f9b5cf9be48a95b44e32b8a9e99b33d6c
jasminaaa20 <jasminaaa.20@uom.lk>
2024-11-13 17:15:03 +0530

    integrate shadcn_ui for enhanced theming and UI components

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:prayer_time/pages/prayer_times_page.dart';
 import 'package:prayer_time/providers/madhab_provider.dart';
 import 'package:provider/provider.dart';
@@ -17,19 +18,27 @@ class PrayerTimesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => MadhabProvider(),
-      child: MaterialApp(
+      child: ShadApp.material(
         title: 'Prayer Times',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.cyan,
-          fontFamily: 'Roboto',
-          textTheme: const TextTheme(
-            bodyLarge: TextStyle(fontSize: 24),
-            bodyMedium: TextStyle(fontSize: 18),
-            bodySmall: TextStyle(fontSize: 14),
-            titleLarge: TextStyle(fontSize: 29, fontWeight: FontWeight.bold),
+        theme: ShadThemeData(
+          colorScheme: const ShadZincColorScheme.light(), brightness: Brightness.light,
+          textTheme: ShadTextTheme(
+            family: 'Roboto',
+            h1: const TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
           ),
         ),
+
+        // theme: ThemeData(
+        //   primarySwatch: Colors.cyan,
+        //   fontFamily: 'Roboto',
+        //   textTheme: const TextTheme(
+        //     bodyLarge: TextStyle(fontSize: 24),
+        //     bodyMedium: TextStyle(fontSize: 18),
+        //     bodySmall: TextStyle(fontSize: 14),
+        //     titleLarge: TextStyle(fontSize: 29, fontWeight: FontWeight.bold),
+        //   ),
+        // ),
         home: const PrayerTimesPage(),
       ),
     );
